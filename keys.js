4e0a1ccc2e86f70b0b89a02e16ec6e757f1f286d
Martin Soengas <martinsoengas@MacBook-Pro.local>
2020-11-26 14:43:03 -0300

    changed .gitignore

--- keys.js
+++
@@ -1,7 +0,0 @@
-const API_KEY_Weather = '26018ba44022eee9c7b6c8f718be387e'
-const API_KEY_Mapbox = 'pk.eyJ1IjoibWFydGluZnMiLCJhIjoiY2toNW5sZDJrMGo4aTJ4bzhmeTJ0YWxsOCJ9.r1gROuUk_m06M6DuWfWx8A'
-
-module.exports = {
-    API_KEY_Weather: API_KEY_Weather,
-    API_KEY_Mapbox: API_KEY_Mapbox
-}
